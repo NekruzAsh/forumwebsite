6d29ab1de2613b189b8591b02cd90e4d98cc691b
Nekruz Ashrapov <64292629+NekruzAsh@users.noreply.github.com>
2023-12-01 18:43:06 -0500

    fix duplicate accounts problem, converted data to json format.

--- index.js
+++ index.js
@@ -1,71 +1,76 @@
-const express = require('express');
-const path = require('path');
-const bcrypt = require('bcrypt');
-const collection = require('./config');
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const collection = require("./config");
 
 const app = express();
-
+//convert data into json format
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
 
+//use EJS as view engine
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+//static file
 
+app.use(express.static("public"));
 
-app.use(express.static('public'));
-
-
-
-app.get('/', (req, res) => {
-
-    res.render('login.ejs');
-
-
-
+app.get("/", (req, res) => {
+  res.render("login.ejs");
 });
 
-
-app.get('/signup', (req, res) => {
-
-    res.render('signup.ejs');
-
+app.get("/signup", (req, res) => {
+  res.render("signup.ejs");
 });
 
 // Register User
-app.post('/signup', async (req, res) => {
+app.post("/signup", async (req, res) => {
+  const data = {
+    name: req.body.username,
 
-    const data = {
+    password: req.body.password,
+  };
 
-        name: req.body.username,
+  //checking if the user already exists
 
-        password: req.body.password
+  const existingUser = await collection.findOne({ name: data.name });
+  if (existingUser) {
+    res.send("User already exists. Please choose another username. ");
+  } else {
+    // hashing the password
 
-    }
+    const saltRounds = 10; // # of salt rounds to use when generating salt
+    const hashedPassword = await bcrypt.hash(data.password, saltRounds);
 
-    //checking if the user already exists
-
-    const existingUser = await collection.findOne({ name: data.name });
-    if(existingUser){
-
-        res.send("User already exists. Please choose another username. "); 
-
-    }else{
-        // hashing the password
-
-        const saltRounds = 10; // # of salt rounds to use when generating salt
-        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
-        const userdata = await collection.insertMany(data);
-        console.log(userdata);
-
-    }
-
+    data.password = hashedPassword;
+    const userdata = await collection.insertMany(data);
+    console.log(userdata);
+  }
 });
 
-
+//login user
+app.post("/login", async (req, res) => {
+  try {
+    const check = await collection.findOne({ name: req.body.username });
+    if (!check) {
+      res.send("User not found. Please register first.");
+    }
+    //compare the hashed password from database
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      check.password
+    );
+    if (validPassword) {
+      res.render("home");
+    } else {
+      req.send("Incorrect Password");
+    }
+  } catch {
+    res.send("wrong credentials");
+  }
+});
 
 app.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+  console.log("Server is listening on port 3000");
 });
-
-
