306da80f3adf92ac9028cb4552f794d6cd2eb559
Nekruz Ashrapov <64292629+NekruzAsh@users.noreply.github.com>
2023-12-01 14:19:43 -0500

    Added password hashing with bcrypt and connected MongoDB for account creation.

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const path = require('path');
 const bcrypt = require('bcrypt');
-
+const collection = require('./config');
 
 const app = express();
 
-//use EJS as view engine
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false }));
+
 
 app.set('view engine', 'ejs');
-//static file
+
 
 app.use(express.static('public'));
 
@@ -29,7 +32,40 @@ app.get('/signup', (req, res) => {
 
 });
 
+// Register User
+app.post('/signup', async (req, res) => {
+
+    const data = {
+
+        name: req.body.username,
+
+        password: req.body.password
+
+    }
+
+    //checking if the user already exists
+
+    const existingUser = await collection.findOne({ name: data.name });
+    if(existingUser){
+
+        res.send("User already exists. Please choose another username. "); 
+
+    }else{
+        // hashing the password
+
+        const saltRounds = 10; // # of salt rounds to use when generating salt
+        const hashedPassword = await bcrypt.hash(data.password, saltRounds);
+        const userdata = await collection.insertMany(data);
+        console.log(userdata);
+
+    }
+
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server is listening on port 3000');
 });
+
+
