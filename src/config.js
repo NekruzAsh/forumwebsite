f4ce434200ba61370e71ff36277395b95d55b849
NekruzAsh <nekruzashrapov16@gmail.com>
2023-12-11 13:55:36 -0500

    updated the env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
-const connect = mongoose.connect(process.env.mongoURI);
+const connect = mongoose.connect(process.env.mongoURL);
 
 connect
   .then(() => {
